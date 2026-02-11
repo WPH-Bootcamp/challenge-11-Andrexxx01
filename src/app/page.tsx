6be0e87328502ab9aec0165a6d307ff4a95377d1
Andrexxx01 <kurniawan.andre50@gmail.com>
2026-02-11 18:42:18 +0700

    Complete Assignment 11 WPH Batch Rendang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { MusicPlayer } from "@/components/MusicPlayer";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50 p-16">
+    <div className="flex min-h-screen items-center justify-center bg-neutral-950 p-16 border border-[#FFFFFF]/10">
       <MusicPlayer />
     </div>
   );
